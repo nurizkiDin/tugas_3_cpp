d7f64911c9a26807b5923cfcd8d793ee875961bd
Nurizki <nutrisi1udin@gmail.com>
2022-10-04 06:14:18 +0700

    tugas 3 cpp
    tugas niomic

---
+++ tugas3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 10;
+	int b = 20;
+	int c = a*b;
+	cout<<"Nilai Variabel A : "<<a<<endl;
+	cout<<"Nilai Variabel B : "<<b;
+	cout<<"\nHasil Perkalian Kedua Variabel Adalah : "<<c;
+}
